303edea88f11b732f219058fcacecc5f307d3463
Renatuscape <silarcta@gmail.com>
2023-11-07 16:12:36 +0100

    Added Flip method to toggle whether card is face up.

--- AW-Blackjack/Card.cs
+++ AW-Blackjack/Card.cs
@@ -4,11 +4,22 @@
     {
         public Suit Suit { get; }
         public Value Value { get;}
-        public bool IsFaceUp { get; set; }
+        public bool IsFaceUp { get; private set; }
         public Card(Suit suit, Value value)
         {
             Suit = suit;
             Value = value;
+        }
+        public void Flip()
+        {
+            IsFaceUp = !IsFaceUp;
+        }
+        public override string ToString()
+        {
+            if (IsFaceUp)
+                return $"{Value} of {Suit}";
+            else
+                return "Facedown Card";
         }
     }
 }
