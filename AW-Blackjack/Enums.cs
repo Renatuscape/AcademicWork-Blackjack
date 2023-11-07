05e535a216e175ac9ff2e2ea0419841b50662305
Renatuscape <silarcta@gmail.com>
2023-11-07 15:14:36 +0100

    Created separate file to hold public enums.

---
+++ AW-Blackjack/Enums.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AW_Blackjack
+{
+    public enum Suit
+    {
+        Spades,
+        Diamonds,
+        Hearts,
+        Clubs
+    }
+    public enum Value
+    {
+        None,
+        Ace,
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Jack,
+        Queen,
+        King
+    }
+}
