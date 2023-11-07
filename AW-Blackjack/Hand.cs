737b8a1e17bde571a1c5aa16767dd5c8668a3e2f
Renatuscape <silarcta@gmail.com>
2023-11-07 15:39:19 +0100

    Created Hand class to hold drawn cards and player information.

---
+++ AW-Blackjack/Hand.cs
@@ -0,0 +1,13 @@
+﻿namespace AW_Blackjack
+{
+    public class Hand
+    {
+        public Role Role { get; set; }
+        public List<Card> Cards { get; set; } = new();
+
+        public Hand(Role role)
+        {
+            Role = role;
+        }
+    }
+}
