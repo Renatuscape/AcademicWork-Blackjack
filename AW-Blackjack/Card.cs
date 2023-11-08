79d11e1a3ae5da9e392accab63662cf55ece566a
Renatuscape <silarcta@gmail.com>
2023-11-08 11:56:05 +0100

    Added override Equals() for unit testing.

--- AW-Blackjack/Card.cs
+++ AW-Blackjack/Card.cs
@@ -3,7 +3,7 @@
     public class Card
     {
         public Suit Suit { get; }
-        public Value Value { get;}
+        public Value Value { get; }
         public bool IsFaceUp { get; private set; }
         public Card(Suit suit, Value value)
         {
@@ -20,6 +20,29 @@
                 return $"{Value} of {Suit}";
             else
                 return "Facedown Card";
+        }
+
+        public bool Equals(Card other)
+        {
+            return Suit == other.Suit && Value == other.Value;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is Card other)
+            {
+                return Equals(other);
+            }
+
+            else
+            {
+                return false;
+            }
+
+        }
+        public override int GetHashCode()
+        {
+            throw new NotImplementedException();
         }
     }
 }
