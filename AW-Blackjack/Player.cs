920c0eb6ed49612eaa47e6c6c4183f3fb310ce25
Renatuscape <silarcta@gmail.com>
2023-11-08 13:50:34 +0100

    Renamed Hand to Player and moved render method to Card.

---
+++ AW-Blackjack/Player.cs
@@ -0,0 +1,13 @@
+﻿namespace AW_Blackjack
+{
+    public class Player
+    {
+        public Role Role { get; set; }
+        public List<Card> Cards { get; set; } = new();
+
+        public Player(Role role)
+        {
+            Role = role;
+        }
+    }
+}
