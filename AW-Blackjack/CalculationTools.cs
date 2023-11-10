9d4f9efbde21ff573304b76dc12475ae5d32140c
Renatuscape <silarcta@gmail.com>
2023-11-10 11:38:10 +0100

    Added separate class for maths operations.

---
+++ AW-Blackjack/CalculationTools.cs
@@ -0,0 +1,32 @@
+﻿namespace AW_Blackjack
+{
+    public static class CalculationTools
+    {
+        public static int CalculateTotal(List<Card> hand)
+        {
+            int sum = 0;
+
+            if (hand.Count == 2)
+            {
+                if ((hand[0].Value == Value.Ace && hand[1].Value > (Value)9) || (hand[1].Value == Value.Ace && hand[0].Value > (Value)9))
+                {
+                    return 21;
+                }
+            }
+
+            for (int i = 0; i < hand.Count; i++)
+            {
+                if (hand[i].Value > (Value)10)
+                {
+                    sum += 10;
+                }
+                else
+                {
+                    sum += (int)hand[i].Value;
+                }
+            }
+
+            return sum;
+        }
+    }
+}
