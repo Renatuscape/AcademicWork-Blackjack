aac0945c942ca6443d4988cf75be18bf6d3f8643
Renatuscape <silarcta@gmail.com>
2023-11-14 13:55:41 +0100

    Added input validatin for player count.

--- AW-Blackjack/Menu.cs
+++ AW-Blackjack/Menu.cs
@@ -4,13 +4,15 @@
     {
         public static void Initialise()
         {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
             while (true)
             {
-                Render.Write("Choose game: (only BlackJack for now)");
+                Render.WriteColouredText("\t ♠  ♦  ♣  ♥ WELCOME TO", ConsoleColor.Black, ConsoleColor.Gray, false);
+                    Render.WriteColouredText(" BLACKJACK ♠  ♦  ♣  ♥ ", ConsoleColor.DarkRed, ConsoleColor.Gray, true, false);
                 Render.Write("");
-                Render.WriteColouredText("\t[0] BlackJack", ConsoleColor.Cyan);
-                Render.WriteColouredText("\t[1] Hi-Lo", ConsoleColor.Cyan);
-                Render.WriteColouredText("\t[X] Exit", ConsoleColor.Magenta);
+                Render.WriteColouredText("\t[0] Start", ConsoleColor.Gray);
+                Render.WriteColouredText("\t[X] Exit", ConsoleColor.Red);
                 Render.Write("");
                 Render.Write("\t", false);
                 var choice = Console.ReadKey().KeyChar.ToString();
@@ -18,13 +20,17 @@
                 {
                     break;
                 }
+                if (choice.ToLower() == "0")
+                {
+                    Console.Clear();
+                    BlackJackInterface();
+                }
                 Console.Clear();
-                BlackJackInterface();
             }
         }
         public static void BlackJackInterface()
         {
-            Render.Write("Welcome to BlackJack! (1 - 4 players)");
+            Render.Write("This game is designed for 1 - 5 players.");
             Render.Write("Please enter number of players:");
             Render.Write("", false);
             var choice = Console.ReadKey().KeyChar.ToString();
@@ -32,14 +38,22 @@
 
             if (int.TryParse(choice, out var playerCount))
             {
-                BlackJackGamePlay game = new();
-                game.NewGame(playerCount);
+                if (playerCount < 1 || playerCount > 5)
+                {
+                    Render.Write("Players must be between 1 and 5.", false);
+                    Render.ContinueAfterInput();
+                }
+                else
+                {
+                    BlackJackGamePlay game = new();
+                    game.NewGame(playerCount);
+                }
+
             }
             else
             {
                 Render.Write("No valid player choice detected. Press any key to return to menu.", false);
-                Console.ReadKey();
-                Console.Clear();
+                Render.ContinueAfterInput();
             }
         }
     }
